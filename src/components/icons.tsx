7f6602c4b33fcbdfd6a05aec88ed085a7588b238
johnwalley <john@walley.org.uk>
2024-05-25 14:51:18 +0100

    Logo!

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,23 +3,49 @@ type IconProps = React.HTMLAttributes<SVGElement>;
 export const Icons = {
   logo: (props: IconProps) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" {...props}>
-      <rect width="256" height="256" fill="none" />
-      <line
-        x1="208"
-        y1="128"
-        x2="128"
-        y2="208"
+      <rect x="0" y="0" width="256" height="256" fill="none" />
+      <path
+        d="M208,128L128,208"
         fill="none"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="16"
       />
-      <line
-        x1="192"
-        y1="40"
-        x2="40"
-        y2="192"
+      <path
+        d="M208,128L166,128"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
+      />
+      <path
+        d="M208,128L208,170"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
+      />
+      <path
+        d="M192,40L150,40"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
+      />
+      <path
+        d="M192,40L192,82"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
+      />
+      <path
+        d="M192,40L40,192"
         fill="none"
         stroke="currentColor"
         strokeLinecap="round"
